c9cc4ca86240d2a640933ecc53a69a00ca8d52ea
alek <wait82985@gmail.com>
2023-11-01 14:43:15 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Solution {
     public int[] findMode(TreeNode root) {
-        return null;
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        int[] max = new int[1];
+        dfs(root, map, max);
+        return map.entrySet().stream().filter(a -> a.getValue() == max[0]).mapToInt(Map.Entry::getKey).toArray();
+    }
+    private void dfs(TreeNode root, Map<Integer, Integer> map, int[] max){
+        if(root == null){
+            return;
+        }
+        System.out.println(root.val);
+        int count = map.getOrDefault(root.val, 0);
+        count++;
+
+        map.put(root.val, count);
+        max[0] = Math.max(max[0], count);
+        if(root.left != null){
+            dfs(root.left, map, max);
+        }
+
+        if(root.right != null){
+            dfs(root.right, map, max);
+        }
+
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -19,5 +19,20 @@ public class SolutionTest {
         int[] actual = new Solution().findMode(root);
 
         Assert.assertArrayEquals(expected, actual);
+    }@Test
+    public void findModeTest3(){
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(0);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(0);
+        root.left.right = new TreeNode(0);
+        root.right.left = new TreeNode(1);
+        root.right.right = new TreeNode(1);
+        root.left.left.left = new TreeNode(0);
+
+        int[] expected = {0,1};
+        int[] actual = new Solution().findMode(root);
+
+        Assert.assertArrayEquals(expected, actual);
     }
 }
